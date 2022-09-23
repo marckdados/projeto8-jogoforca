e2380fa67efe4ce51ad7815b680bb84ccf4f3bb9
bis <bis@pop-os.localdomain>
2022-09-23 17:04:50 -0300

    Terminando layout, faltando só a lógica do jogo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import imagem from "./assets/forca0.png";
 import styled from "styled-components";
+import array from "./alfabeto";
 export default function App() {
   return (
     <Container>
@@ -10,7 +11,16 @@ export default function App() {
           <span>ABCDERFGHI</span>
         </Palavra>
       </Topo>
-      <Botoes></Botoes>
+      <Botoes>
+        {array.map((letra, index) => (
+          <div key={index}>{letra.toUpperCase()}</div>
+        ))}
+      </Botoes>
+      <CaixaInput>
+        <span>Já sei a palavra !</span>
+        <input placeholder="" />
+        <button>Chutar</button>
+      </CaixaInput>
     </Container>
   );
 }
@@ -35,6 +45,7 @@ const Topo = styled.div`
 const Palavra = styled.div`
   width: 40%;
   display: flex;
+  margin: 20px 0;
   flex-direction: column;
   justify-content: space-between;
 
@@ -44,7 +55,6 @@ const Palavra = styled.div`
     background-color: #44c448;
     border-radius: 6px;
     border: none;
-
     font-weight: 700;
     font-size: 1.2em;
     color: #ffffff;
@@ -52,16 +62,51 @@ const Palavra = styled.div`
 `;
 
 const Botoes = styled.div`
-  width: 70%;
+  width: 50%;
   display: flex;
   flex-wrap: wrap;
+  margin-top: 50px;
 
   div {
     width: 50px;
     height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: #e1ecf4;
     border-radius: 6px;
     border: 1px solid #93b8d1;
     margin: 10px 10px;
+    font-weight: 700;
+    color: #93b8d1;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #608096;
+    }
+  }
+`;
+
+const CaixaInput = styled.div`
+  width: 50%;
+  display: flex;
+  margin-top: 50px;
+  justify-content: space-evenly;
+  align-items: center;
+
+  input {
+    width: 30%;
+    height: 30px;
+    border-radius: 3px;
+  }
+
+  button {
+    width: 10%;
+    background-color: #e1ecf4;
+    color: #39739d;
+    font-weight: 700;
+    border-radius: 6px;
+    border: 1px solid #93b8d1;
+    height: 40px;
   }
 `;
